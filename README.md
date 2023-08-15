082139cefb5312b2028d6a88b95b9f5191e5daab
Orkhan Iskenderov <orkhan-iskenderov@yandex.ru>
2023-08-16 00:35:08 +0300

    Добавил IP адрес в README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # movies-explorer-api
+
+Адрес репозитория: https://github.com/Orkhan777/movies-explorer-api/tree/level-1
+
+## Ссылки на проект
+
+IP-адрес 84.201.134.117
+
+Backend  https://api.movie-project.nomoreparties.co
+
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,8 @@
 const corsOptions = {
   origin: [
-    'http://movies-project.nomoreparties.co',
+    'movies-project.nomoreparties.co',
     'http://localhost:3000',
+    'localhost:3000',
   ],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
