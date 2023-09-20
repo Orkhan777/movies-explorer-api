4175876ae94157156860470031240139b72408fe
Orkhan Iskenderov <orkhan-iskenderov@yandex.ru>
2023-09-20 20:49:00 +0300

    Изменение порта

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const limiter = require('./middlewares/limiter');
 const corsOptions = require('./middlewares/cors');
 
 const {
-  NODE_ENV = 'development', DB_PRODUCTION, PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  NODE_ENV = 'development', DB_PRODUCTION, PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
 const app = express();
